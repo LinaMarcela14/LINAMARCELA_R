cbcdc8363d08ba17b6aebe4feccab3b0ac796053
LinaMarcela14 <lina.roldan@est.iudigital.edu.co>
2025-03-15 17:20:44 +0000

    carga

--- setup.py
+++ setup.py
@@ -10,11 +10,13 @@ setup(
     description="",
     py_modules=["actividad_1", "actividad_2"],
     install_requires=[
+        "kagglehub[pandas-datasets]>=0.3.8",
+        "matplotlib>=3.5.0",
+        "seaborn>=0.11.2",
         "pandas",
         "requests",
         "numpy",
         "openpyxl",
-        "matplotlib"
     ]
     
     
